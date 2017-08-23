117f0a45f4c629c4201030b91a8700bfadfcc43a
Said Benaissa <d.sbenaissa@gmail.com>
2017-08-23 23:46:44 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+/**
+ * Created by benaissa on 8/23/17.
+ */
+public class Main {
+    public static void main(String [] args)
+    {
+        System.out.println("Array of primitives");
+//        int ints[] = {3, 6, 9};
+        int ints[] = {9, 6, 3};
+        Arrays.sort(ints);
+        for (int i = 0; i <ints.length ; i++) {
+            System.out.println(ints[i]);
+        }
+        System.out.println("Array of Strings");
+        String string[] = {"Red", "Green", "Blue"};
+        Arrays.sort(string);
+        for (String color :string){
+            System.out.println(color);
+        }
+
+        System.out.println("Setting an initial size");
+        int sized[] = new int[10];
+
+        for (int i = 0; i <sized.length ; i++) {
+            sized[i] = i*100;
+        }
+
+        for (int value:sized)
+        {
+            System.out.println(value);
+        }
+
+        System.out.println("Copying an array");
+        int copied[] =new int[5];
+        System.arraycopy(sized,5,copied,0,5);
+
+        for (int value:copied){
+            System.out.println(value);
+        }
+    }
+}
